2b9416ef53101260382642f1e42269da4774983b
Mathieu Porretta <mathieu.porretta@mastertp>
2018-11-16 12:13:16 +0100

    network

--- app/src/main/java/fr/mvproduction/svetxav/todaysprogram/MainActivity.java
+++ app/src/main/java/fr/mvproduction/svetxav/todaysprogram/MainActivity.java
@@ -4,10 +4,20 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private SensorManager sensorManager;
@@ -17,11 +27,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     private TextView textSensorX;
     private TextView textSensorY;
     private TextView textSensorZ;
-
     private TextView textMain;
 
-    public MainActivity(){
+    private EditText textInput;
+    private String message;
 
+    private static final String IP = "192.168.2.148";
+    private static final int PORT = 8081;
+    private InetAddress address;
+    private DatagramSocket UDPSocket;
+
+    public MainActivity(){
+        this.message="";
     }
 
     @Override
@@ -38,12 +55,31 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         this.textSensorY = findViewById(R.id.text_sensor_output_y);
         this.textSensorZ = findViewById(R.id.text_sensor_output_z);
 
+        this.textInput = findViewById(R.id.main_text_input);
+
         findViewById(R.id.main_button).setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 textMain.setText(R.string.bt_click_message);
+                message = textInput.getText().toString();
+                sendNetworkMessage(message);
             }
         });
+    }
+
+    protected void sendNetworkMessage(final String str){
+        (new Thread(){
+            public void run(){
+                try {
+                    byte[] data = str.getBytes();
+                    DatagramPacket packet = new DatagramPacket(data, data.length, address, PORT);
+                    UDPSocket.send(packet);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
     }
 
     @Override
@@ -57,6 +93,18 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         super.onResume();
         this.sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
         this.sensorManager.registerListener(this, proximity, SensorManager.SENSOR_DELAY_UI);
+
+        // init network
+        try {
+            this.UDPSocket = new DatagramSocket();
+            this.address = InetAddress.getByName(IP);
+
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
     }
 
     @Override
@@ -75,4 +123,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public void onAccuracyChanged(Sensor sensor, int accuracy) {
 
     }
+
+//    private class ReceiverTask extends AsyncTask<Void byte[], Void>{
+//
+//    }
 }
